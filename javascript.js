4b7e85af9b6e0c0cfc14efd393f2393138d94314
Santtu-Siren-School <santtu.siren@edu.hyria.fi>
2025-12-03 11:28:40 +0200

    code to the project

---
+++ javascript.js
@@ -0,0 +1,23 @@
+document.getElementById("submit").addEventListener("click", function() {
+    feedback();
+    console.log("clicked")
+});
+function feedback() {
+    const feedback=document.getElementById("feedback");
+    let output="";
+    console.log("function")
+    let q1=document.getElementById('soul').value.trim().toLowerCase();
+    let q2=document.getElementById('money').value.trim().toLowerCase();
+    let q3=document.getElementById('car').value.trim().toLowerCase();
+    let q4=document.getElementById('house').value.trim().toLowerCase();
+    if(q1==="yes"&&q2==="yes"&&q3==="yes"&&q4==="yes") {
+        const p = document.createElement("p");
+        p.textContent = "Thank you pookie bear :3 UwU";
+        feedback.appendChild(p);
+    }
+    else {
+        const p = document.createElement("p");
+        p.textContent = "you should burn in the deepest pits of hell for thousend years for your crimes";
+        feedback.appendChild(p);
+    }
+}
